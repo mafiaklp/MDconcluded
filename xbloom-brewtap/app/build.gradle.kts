33908109469ba84428fc4e40a06b65bea8371038
mafiaklp <mafiaklp@gmail.com>
2026-08-15 00:14:07 +0700

    chore: bump BrewTap to 1.3.1

--- xbloom-brewtap/app/build.gradle.kts
+++ xbloom-brewtap/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.brewtap.xbloom"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.3.0"
+        versionCode = 31
+        versionName = "1.3.1"
     }
 
     buildTypes {
