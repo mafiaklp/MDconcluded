42ec5bf1a36066921668c09a6f54e014667baed2
mafiaklp <mafiaklp@gmail.com>
2026-08-15 01:12:07 +0700

    build: bump BrewTap NFC writer to 1.5.0

--- xbloom-brewtap/app/build.gradle.kts
+++ xbloom-brewtap/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.brewtap.xbloom"
         minSdk = 26
         targetSdk = 35
-        versionCode = 40
-        versionName = "1.4.0"
+        versionCode = 50
+        versionName = "1.5.0"
     }
 
     signingConfigs {
