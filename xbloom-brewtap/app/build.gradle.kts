ec3cb9b0ed8f6642126d294bb3ed747b6ead3ea1
mafiaklp <mafiaklp@gmail.com>
2026-08-15 09:07:29 +0700

    build: bump BrewTap to 1.5.1

--- xbloom-brewtap/app/build.gradle.kts
+++ xbloom-brewtap/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.brewtap.xbloom"
         minSdk = 26
         targetSdk = 35
-        versionCode = 50
-        versionName = "1.5.0"
+        versionCode = 51
+        versionName = "1.5.1"
     }
 
     signingConfigs {
